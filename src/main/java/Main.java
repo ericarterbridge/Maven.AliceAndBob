26d3b2c93a28db084a9fdf788eb795bfe06791c2
Eric <eric@zipcoders-mbp-6.myfiosgateway.com>
2022-06-29 21:49:04 -0400

    completed lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+        Scanner getName = new Scanner(System.in);
+        System.out.println("Please enter your name.");
+        String userName = getName.nextLine();
+        if (userName.toLowerCase().equals("alice")){
+            System.out.println("Hello Alice!  We've been waiting for you.");
+        } else if (userName.toLowerCase().equals("bob")){
+            System.out.println("Hello Bob!  We've been waiting for you.");
+        } else {
+            System.out.println("Please go away from here.  We only wish to speak with Alice and Bob.");
+        }
+
 
     }
 }
